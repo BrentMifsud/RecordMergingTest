11039bc005b3242d92d63ac5b348d3a466503791
Brent Mifsud <brent.mifsud@outlook.com>
2019-07-06 00:54:18 -0400

    use commons file utilities to get extension

--- src/main/java/com/brentmifsud/domain/FileType.java
+++ src/main/java/com/brentmifsud/domain/FileType.java
@@ -1,23 +1,14 @@
 package com.brentmifsud.domain;
 
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.io.FilenameUtils;
 
 public enum FileType {
     HTML,
     CSV;
 
-    public static Boolean isSupportedFileType(String fileName) throws IllegalArgumentException {
-        //Split file name and extension
-        String[] splitString = StringUtils.split(fileName, '.');
-
-        //Return false if file has no extension
-        if(splitString.length == 1) return false;
-
-        //Grab the last extension (this is for edge cases where a file has multiple '.' in its name)
-        String extension = splitString[splitString.length - 1];
-
+    public static Boolean isSupportedFileType(String fileName) {
         try {
-            FileType.valueOf(extension.toUpperCase());
+            FileType.valueOf(FilenameUtils.getExtension(fileName).toUpperCase());
             return true;
         } catch (IllegalArgumentException e) {
             return false;
