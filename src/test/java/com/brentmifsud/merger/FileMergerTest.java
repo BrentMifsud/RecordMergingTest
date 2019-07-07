3bf04807232862947076167dbee42aaec720cc7c
Brent Mifsud <brent.mifsud@outlook.com>
2019-07-06 23:45:18 -0400

    add test case for prepareInputFiles

---
+++ src/test/java/com/brentmifsud/merger/FileMergerTest.java
@@ -0,0 +1,22 @@
+package com.brentmifsud.merger;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FileMergerTest {
+
+    public static final FileMerger subject = new FileMerger();
+    public static String[] input = {"src/test/resources/first.html","src/test/resources/second.csv"};
+
+    @Test
+    void prepareInputFiles() {
+        List<File> result = subject.prepareInputFiles(input);
+        for (File file:result){
+            assertThat(file.canRead()).isEqualTo(true);
+        }
+    }
+}
